73efcafd80abad4cf9a844ccdb1194b750f48961
msteele96 <marcsteele2014@gmail.com>
2021-03-17 14:06:40 -0400

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,39 @@
+class ArtistsController < ApplicationController
+    
+    def index
+        @artists = Artist.all
+    end
+
+    def show
+        @artist = Artist.find_by(id: params[:id])
+    end
+
+    def new
+        @artist = Artist.new
+    end
+
+    def create
+        @artist = Artist.new
+        @artist.update(artist_params)
+        @artist.save
+        redirect_to artist_path(@artist)
+    end
+
+    def edit
+        @artist = Artist.find_by(id: params[:id])
+    end
+
+    def update
+        @artist = Artist.find_by(id: params[:id])
+        @artist.update(artist_params)
+        @artist.save
+        redirect_to artist_path(@artist)
+    end
+
+    private
+
+    def artist_params
+        params.require(:artist).permit!
+    end
+    
+end
